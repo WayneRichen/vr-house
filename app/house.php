d99541c1c8b8e432e236db3681a5255b3804c793
Wayne Richen <wayne.richen@gmail.com>
2022-07-05 14:07:13 +0800

    Landlord info

--- app/house.php
+++ app/house.php
@@ -1,9 +1,10 @@
 <?php
 if (isset($_GET['id'])) {
     require('db.php');
-    $sql = 'SELECT *
-            FROM house
-            WHERE `id` = :id AND `enable` = 1';
+    $sql = 'SELECT `house`.*, `landlord`.`name` as `landlord_name`, `landlord`.`email` as `landlord_email`, `landlord`.`phone` as `landlord_phone` 
+            FROM `house`
+            LEFT JOIN `landlord` ON `landlord`.`id` = `house`.`landlord`
+            WHERE `house`.`id` = :id AND  `house`.`enable` = 1';
     $sth = $conn->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
     $sth->execute(array('id' => $_GET['id']));
     $house = $sth->fetch();
--- public/house.php
+++ public/house.php
@@ -11,6 +11,7 @@ require APP_PATH . 'house.php'
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="./static/css/output.css" rel="stylesheet">
   <script src="./static/js/jquery-3.6.0.min.js"></script>
+  <link rel="stylesheet" href="./static/js/flowbite.min.css" />
   <title><?= $house['title'] ?> | 看房網</title>
 </head>
 <div class="w-full h-screen">
@@ -20,13 +21,39 @@ require APP_PATH . 'house.php'
       <img src="<?= $house['images'][0] ?>" class="w-1/2 mx-auto">
     </div>
     <span class="text-3xl font-bold text-gray-700"><?= $house['title'] ?></span>
-    <p class="text-xl text-gray-700"><?= $house['subtitle'] ?>
-    <p class="mt-2 whitespace-pre"><?= $house['description'] ?></p>
+      <p class="text-xl text-gray-700"><?= $house['subtitle'] ?>
+      <p class="mt-2 whitespace-pre-line	"><?= $house['description'] ?></p>
   </div>
   <div class="fixed bottom-0 w-full h-16 bg-white shadow-inner">
     <div class="max-w-screen-lg mx-auto h-full flex justify-end items-center">
       <span class="text-lg font-bold mr-4 text-red-600"><?= $house['rent'] ?>&nbsp;&frasl;&nbsp;一個月</span>
-      <a href="<?= $house['vr_url'] ?>"><button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-bold text-lg rounded-lg px-5 py-2.5 text-center mr-2">VR 線上看房</button></a>
+      <a href="<?= $house['vr_url'] ?>" target="_blank"><button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-bold text-lg rounded-lg px-5 py-2.5 text-center mr-2">VR 線上看房</button></a>
+      <button type="button" data-tooltip-target="tooltip-default" class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center ml-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800">聯絡房東</button>
+      <div id="tooltip-default" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
+        房東姓名：<?= $house['landlord_name'] ?><br>房東手機：<?= $house['landlord_phone'] ?><br>房東 Email：<?= $house['landlord_email'] ?>
+        <div class="tooltip-arrow" data-popper-arrow></div>
+      </div>
     </div>
   </div>
 </div>
+
+<script src="./static/js/flowbite.js"></script>
+<script>
+const targetEl = document.getElementById('tooltip-default');
+const triggerEl = document.getElementById('tooltipButton');
+
+// options with default values
+const options = {
+  placement: 'bottom',
+  triggerType: 'hover',
+  onHide: () => {
+      console.log('tooltip is shown');
+  },
+  onShow: () => {
+      console.log('tooltip is hidden');
+  }
+};
+$('#region').on('change', function () {
+  this.form.submit();
+});
+</script>
