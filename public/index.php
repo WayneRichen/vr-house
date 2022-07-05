34bef022964452519316af3becbefc2cc8edeadd
Wayne Richen <wayne.richen@gmail.com>
2022-07-05 14:09:46 +0800

    Change slide sequence

--- public/index.php
+++ public/index.php
@@ -16,21 +16,21 @@ session_start();
 <div class="w-full min-h-screen">
   <div class="slideshow-container">
     <div class="mySlides fade">
+      <a href="./faq.php"><img src="./static/image/slide3.png" style="width:100%"></a>
+    </div>
+
+    <div class="mySlides fade">
       <img src="./static/image/slide1.jpg" style="width:100%">
     </div>
 
     <div class="mySlides fade">
       <img src="./static/image/slide2.jpg" style="width:100%">
     </div>
-
-    <div class="mySlides fade">
-      <a href="./faq.php"><img src="./static/image/slide3.png" style="width:100%"></a>
-    </div>
   </div>
 </div>
 
 <script>
-  let slideIndex = 1;
+  let slideIndex = 0;
   autoSlides();
   function autoSlides() {
     let slides = document.getElementsByClassName("mySlides");
